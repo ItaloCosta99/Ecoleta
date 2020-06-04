410dd07ff055c68b4a95e1d524e9e12a9b65a860
Italo Costa <italocostajobs@gmail.com>
2020-06-04 04:29:17 -0300

    2nd Commit (Day 3 of NLW)

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,3 +1,5 @@
+//Seleção de localidade
+
 function populateUFs() {
     const ufSelect = document.querySelector("select[name=uf]")
     
@@ -27,19 +29,20 @@ function getCities(event) {
         return
     }
 
-    citySelect.innerHTML = "";
-
     const indexOfSelectedState = event.target.selectedIndex
 
     stateInput.value = event.target.options[indexOfSelectedState].text
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
     
+    citySelect.innerHTML = "<option valeu>Selecione a Cidade</option>";
+    citySelect.disabled = true
+
     fetch(url)
     .then( res => res.json() )
     .then( cities => {
         for( const city of cities ) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
 
         citySelect.disabled = false
@@ -50,3 +53,42 @@ function getCities(event) {
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+
+// Itens de coleta
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    const alreadySelected = selectedItems.findIndex( function(item) {
+        const itemFound = item == itemId 
+        return itemFound
+    } )
+
+    if(alreadySelected >= 0) {
+        const filteredItems = selectedItems.filter( item => {
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+        } )
+
+        selectedItems = filteredItems
+    } else {
+        selectedItems.push(itemId)
+    }
+
+    collectedItems.value = selectedItems
+}
