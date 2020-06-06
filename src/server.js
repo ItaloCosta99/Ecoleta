78588043487acc6211aff010dcda5feffdc88ab2
Italo Costa <italocostajobs@gmail.com>
2020-06-05 22:20:45 -0300

    4th Commit (Last day of NLW)

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const server = express()
 
+//Pegar o banco de dados
+const db = require("./database/db")
+
 //Configurar pasta pública
 server.use(express.static("public"))
+
+//Habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({ extended: true}))
 
 //Utilizando template engine
 const nunjucks = require("nunjucks")
@@ -21,11 +27,67 @@ server.get("/create-point", (req, res) => {
     return res.render("create-point.html")
 })
 
-server.get("/search", (req, res) => {
-    return res.render("search-results.html")
+server.post("/savepoint", (req, res) => {
+    //console.log(req.body)
+
+    //Inserir dados no banco de dados
+    const query = `        
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no cadastro")
+        }
+
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.render("create-point.html", {saved: true})
+    }
+
+    db.run(query, values, afterInsertData)
+
 })
 
+server.get("/search", (req, res) => {
+    const search = req.query.search
 
+    if(search == ""){
+        //Pesquisa vazia
+        return res.render("search-results.html", { total: 0})
+    }
+
+    //Pegar os dados do banco de dados
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
+        if (err) {
+            return console.log(err)
+        }
+
+        const total = rows.length
+
+        return res.render("search-results.html", {places: rows, total})
+        })
+})
 
 //Ligar o servidor
 server.listen(3000)
